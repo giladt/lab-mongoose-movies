87858f8f7bc9b0742c9979c9aa740ad20c20ea15
Gilad Tsabar <gilad@tsabar.net>
2020-09-08 20:40:32 +0200

    Done up to iteration 12

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const Celebrity = require('../models/Celebrity');
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -22,7 +23,74 @@ router.get('/celebrities/new', (req, res, next)=>{
   res.render('celebrities/new');
 });
 
-router.post('/celebrities/new', (req, res, next)=>{
+router.get('/movies', (req, res, next)=>{
+  Movie.find()
+  .populate('cast')
+  .then(dbMovies => {
+    res.render('movies/index', {movies: dbMovies});
+  })
+  .catch(err => {
+    next(err);
+  });
+});
+
+router.get('/movies/new', (req, res, next)=>{
+  Celebrity.find()
+    .then(celebrities => {
+      res.render('movies/new', {celebrities});
+    });
+});
+
+router.post('/movies', (req, res, next)=>{
+  const {title, genre, plot, cast} = req.body;
+
+  Movie.create({
+    title, genre, plot, cast
+  })
+  .then(dbMovie => {
+    console.log(`A movie named ${dbMovie.title} has been added to the database.`);
+    res.redirect(`/movies`); //${dbMovie._id}
+  })
+  .catch(err=>{
+    next(err);
+  });
+});
+
+router.get('/movies/:id/edit', (req, res, next)=>{
+  const id = req.params.id;
+  Celebrity.find()
+    .then(dbCelebrities => {
+      Movie.findById(id)
+      .then(movie => {
+        dbCelebrities.forEach(celeb=>{
+          celeb.selected = movie.cast.includes(celeb._id.toString());
+        });
+  
+        res.render('movies/edit', {movie, cast: dbCelebrities});
+      })
+      .catch(err => {
+        next(err);
+      });
+    });
+});
+
+router.post('/movies/:id/edit', (req, res, next)=>{
+  const {title, genre, plot, cast} = req.body;
+  const id = req.params.id;
+
+  Movie.findByIdAndUpdate(id,{
+    title, genre, plot, cast
+  })
+  .then(dbMovie => {
+    console.log(`${dbMovie.title} has been added to the database.`);
+    res.redirect(`/movies`);
+  })
+  .catch(err=>{
+    next(err);
+  });
+});
+
+router.post('/celebrities', (req, res, next)=>{
   const {name, occupation, catchPhrase} = req.body;
   Celebrity.create({
     name, occupation, catchPhrase
